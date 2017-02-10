60e1a7fd46c7ef1fcb32ada1d02ca6cc788adafe
Yihui Xie <xie@yihui.name>
2017-02-09 23:11:03 -0600

    update the build scripts

--- R/build.R
+++ R/build.R
@@ -1,17 +1,46 @@
 options(stringsAsFactors = FALSE)
+cargs = commandArgs(TRUE)
+local = cargs[1] == 'TRUE'
 
-files = read.csv('data/external_Rmd.csv', strip.white = TRUE)
-for (i in seq_len(nrow(files))) {
-  io = unlist(files[i, ])
-  if (!file.exists(io[1]) && Sys.getenv('USER') != 'yihui') {
-    warning('File ', io[1], ' does not exist')
-    next
+build_one = function(io, external = FALSE)  {
+  if (!file.exists(io[1])) {
+    (if (Sys.getenv('USER') == 'yihui') stop else warning)(
+      'File ', io[1], ' does not exist'
+    )
+    return()
   }
   # if output is newer than input, skip the compilation
-  if (file_test('-nt', io[2], io[1])) next
+  if (file_test('-nt', io[2], io[1])) {
+    # if an Rmd is currently staged, compile it even if .md is newer than .Rmd
+    staged = system2('git', 'diff --name-only --cached', stdout = TRUE)
+    if (!io[1] %in% staged) return()
+  }
 
-  if (blogdown:::Rscript(shQuote(c('R/build_one.R', io))) != 0) {
+  if (local) message('* knitting ', io[1])
+  if (blogdown:::Rscript(shQuote(c('R/build_one.R', io, cargs, external))) != 0) {
     unlink(io[2])
     stop('Failed to compile ', io[1], ' to ', io[2])
+  }
+}
+
+# external Rmd files
+files = read.csv('data/external_Rmd.csv', strip.white = TRUE)
+files = cbind(as.matrix(files), external = TRUE)
+# Rmd files under the content directory
+rmds = list.files('content', '[.]Rmd$', recursive = TRUE, full.names = TRUE)
+if (length(rmds)) {
+  files = rbind(files, cbind(rmds, gsub('.Rmd$', '.md', rmds), FALSE))
+}
+
+for (i in seq_len(nrow(files))) {
+  build_one(unlist(files[i, 1:2]), files[i, 3])
+}
+
+blogdown:::hugo_build(local = local)
+
+if (!local) {
+  message('Optimizing PNG files under static/')
+  for (i in list.files('static', '[.]png$', full.names = TRUE, recursive = TRUE)) {
+    system2('optipng', shQuote(i), stderr = FALSE)
   }
 }
--- R/build_one.R
+++ R/build_one.R
@@ -1,20 +1,26 @@
 local({
-  # input/output filenames are passed as two additional arguments to Rscript
+  # input/output filenames are passed as command-line arguments
   a = commandArgs(TRUE)
-  if (length(a) < 2)
-    stop("The script build_one.R requires at least 2 command-line args")
+  if (length(a) < 2) stop(
+    "The script build_one.R requires at least 2 command-line args"
+  )
   unlink(a[2])
 
-  d = knitr:::sans_ext(basename(a[1]))  # 'foo/bar/hi.Rmd' -> 'hi'
+  # for external Rmd, '../../foo/bar/hi.Rmd' -> 'hi'; for internal,
+  # content/foo/bar/hi.Rmd -> foo/bar/hi
+  d = knitr:::sans_ext(
+    if (a[4] == 'TRUE') basename(a[1]) else gsub('^content/', '', a[1])
+  )
   knitr::opts_chunk$set(
-    fig.path   = sprintf('figure/%s/', d),
-    cache.path = sprintf('cache/%s/', d), error = FALSE
+    fig.path   = sprintf('figures/%s/', d),
+    cache.path = sprintf('blogdown/cache/%s/', d),
+    error = FALSE, fig.width = 6, fig.height = 5, dpi = 96, tidy = TRUE
   )
   knitr::opts_knit$set(
-    base.dir = normalizePath('../../Apps/updog/drop/hugo/', mustWork = TRUE),
-    base.url = 'https://db.yihui.name/hugo/'
+    base.dir = normalizePath('static/', mustWork = TRUE),
+    base.url = if (a[3] == 'FALSE') 'https://assets.yihui.name/' else '/',
+    width = 60
   )
-  knitr::opts_knit$set(width = 70)
   knitr::knit(a[1], a[2], quiet = TRUE, encoding = 'UTF-8', envir = .GlobalEnv)
   if (file.exists(a[2])) {
     x = blogdown:::append_yaml(blogdown:::readUTF8(a[2]), list(from_Rmd = TRUE))
