a65a6e8651710a51c05f0238e28a0aba1ca2e118
Yihui Xie <xie@yihui.name>
2022-04-17 00:45:03 -0500

    revert 71ad62eb81b52d8165d11daad1cf2619f57d9728: still use jsdelivr for hosting js and css

--- build.sh
+++ build.sh
@@ -2,34 +2,6 @@
 
 cd static
 
-# clone js/css from a few other repos
-ALL_REPOS="misc.js arith.js zdict.js"
-for i in $ALL_REPOS; do
-  git clone --depth=1 https://github.com/yihui/$i.git
-done
-
-
-# zdict.js needs a custom build
-cd zdict.js
-./build.sh
-cd ..
-
-for i in $ALL_REPOS; do
-  [ -d $i/js ] && cp $i/js/*.js js/
-  [ -d $i/css ] && cp $i/css/*.css css/
-  rm -rf $i
-done
-
-# minify css
-npm install clean-css-cli -g
-cleancss --batch --batch-suffix '.min' css/*.css
-
-# minify js
-npm install terser -g
-for i in js/*.js; do
-  terser $i --compress --mangle --source-map --output ${i%.js}.min.js
-done
-
 # do not index the site on Netlify (only index the one built on Vercel)
 if [ "$NETLIFY" = "true" ]; then
   echo 'User-agent: *
