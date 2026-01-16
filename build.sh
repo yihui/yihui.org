64277fee59bf568ece505feafa19359af522cd38
Yihui Xie <xie@yihui.name>
2026-01-16 14:01:39 -0600

    move to Cloudflare Pages since my site constantly uses 1.7M edge requests per month (passing the 1M limit of free tier)

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-shopt -s dotglob
 
 # do not index the site on Netlify (only index the one built on Vercel)
 if [ "$NETLIFY" = "true" ]; then
@@ -9,19 +8,17 @@ Disallow: /' > robots.txt
   cd ..
 fi
 
-# Vercel only does a shallow clone, which loses git info
-if [ "$VERCEL_ENV" = "production" ]; then
-  rm -rf *
-  git clone --recursive -b $VERCEL_GIT_COMMIT_REF https://github.com/$VERCEL_GIT_REPO_OWNER/$VERCEL_GIT_REPO_SLUG.git .
-  git config advice.detachedHead false
-  git checkout $VERCEL_GIT_COMMIT_SHA
+# Cloudflare Pages only does a shallow clone, which loses git info
+if [ "$CF_PAGES_BRANCH" = "master" ]; then
+  git fetch --unshallow
+  mv static/_redirects_cf static/_redirects
 fi
 
-# Netlify uses $CONTEXT and Vercel uses $VERCEL_ENV
-if [ "${VERCEL_ENV:-$CONTEXT}" = "production" ]; then
+# for preview, build drafts (-D) and use the baseURL from the CI provider
+if [ "${VERCEL_ENV:-$CONTEXT}" = "production" ] || [ "$CF_PAGES_BRANCH" = "master" ]; then
   hugo -F --enableGitInfo
 else
-  hugo -F -D -b ${VERCEL_URL:-$DEPLOY_PRIME_URL} --enableGitInfo
+  hugo -F -D -b "${CF_PAGES_URL:-$DEPLOY_PRIME_URL}" --enableGitInfo
 fi
 
 # add litedown documentation
